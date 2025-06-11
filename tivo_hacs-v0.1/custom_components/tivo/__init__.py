72d6bb536e40f2f89f8b7c689ee27e6d701790e7
Timman70 <timman7035@gmail.com>
2025-06-11 18:32:15 -0400

    Add files via upload

---
+++ tivo_hacs-v0.1/custom_components/tivo/__init__.py
@@ -0,0 +1,8 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "tivo"
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
+    return True
