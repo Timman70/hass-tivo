ac20dc899ad8dd8ccf483869d67a97ca1a4871c9
Timman70 <timman7035@gmail.com>
2025-06-14 18:56:01 -0400

    Delete tivo_hacs-v0.1/custom_components/tivo directory

--- tivo_hacs-v0.1/custom_components/tivo/__init__.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-
-DOMAIN = "tivo"
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
-    return True
